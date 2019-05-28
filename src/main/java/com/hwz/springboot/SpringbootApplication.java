7e41b66d9249ea3eae1fe5b7cb816a2fd415509a
胡文哲 <18675556101@163.com>
2019-05-28 16:31:10 +0800

    j

---
+++ src/main/java/com/hwz/springboot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.hwz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+        System.out.println("1111111111111111111111111111111111");
+    }
+
+}
