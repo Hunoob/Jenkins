82bdc4151b4eb01b0185bad6f133819901f9f990
hunoob <18675556101@163.com>
2019-05-29 20:06:13 +0800

    t

--- src/main/java/com/hwz/springboot/SpringbootApplication.java
+++ src/main/java/com/hwz/springboot/SpringbootApplication.java
@@ -9,6 +9,7 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
         System.out.println("1111111111111111111111111111111111");
+        System.out.println("122222222222222222222222222222222222");
     }
 
 }
